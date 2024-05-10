6c3141a171fa1e849f0d5b084784b2edf6f07a4a
arunthakore <44212275+arunthakore@users.noreply.github.com>
2024-05-10 10:16:38 -0400

    Update streamlit_app.py
    commented session - get_active_session();

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ st.write("""Choose the fruits you want in your custom Smoothie""")
 
 
 
-session = get_active_session()
+#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col("FRUIT_NAME"))
 
 # st.dataframe(data=my_dataframe, use_container_width=True)
